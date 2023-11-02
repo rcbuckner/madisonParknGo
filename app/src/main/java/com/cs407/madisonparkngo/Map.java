710407746c7558dfa55ae211f9ab131e6bf0ddb7
Nathan Labiosa <nlabiosa@wisc.edu>
2023-11-02 18:37:25 -0500

    adding first implementation of map

---
+++ app/src/main/java/com/cs407/madisonparkngo/Map.java
@@ -0,0 +1,96 @@
+package com.cs407.madisonparkngo;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.webkit.PermissionRequest;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.internal.IGoogleMapDelegate;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+public class Map extends FragmentActivity {
+
+    private final LatLng mDestinationLatLng = new LatLng(43.0753,-89.4034);
+    private GoogleMap mMap;
+
+    private FusedLocationProviderClient mfusedLocationProviderClient;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 12;
+
+    private static final int DEFAULT_ZOOM = 15;
+    private final LatLng mDefaultLocation = new LatLng(-33.8523341, 151.2106085); // This should be a sensible default for your app
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_map);
+        mapFragment.getMapAsync(googleMap -> {
+            mMap = googleMap;
+            googleMap.addMarker(new MarkerOptions().position(mDestinationLatLng).title("Destination"));
+            displayMyLocation();
+        });
+
+        mfusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+
+
+    }
+    private void displayMyLocation(){
+        int permission= ActivityCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permission== PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+
+        }
+        else {
+            mfusedLocationProviderClient.getLastLocation().addOnCompleteListener(this, task ->{
+                Location mLastKnownLocation = task.getResult();
+                if (task.isSuccessful() && mLastKnownLocation != null){
+                    mMap.addPolyline(new PolylineOptions().add(
+                            new LatLng(mLastKnownLocation.getLatitude(),mLastKnownLocation.getLongitude()), mDestinationLatLng));
+
+                    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_map);
+                    mapFragment.getMapAsync(googleMap -> {
+                        mMap = googleMap;
+                        googleMap.addMarker(new MarkerOptions().position(new LatLng(mLastKnownLocation.getLatitude(),mLastKnownLocation.getLongitude())).title("Last Known Location"));
+                        displayMyLocation();
+                    });
+                }
+
+
+
+            });
+
+        }
+
+
+    };
+
+    @Override
+    public void onRequestPermissionsResult(int resultCode, String[] permissions, int[] grantResults) {
+
+        super.onRequestPermissionsResult(resultCode, permissions, grantResults);
+        if (resultCode == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                displayMyLocation();
+            }
+        }
+    }
+
+
+
+}
