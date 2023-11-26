f917f1b4fe5ce3e709a4c993aaf643ea47ab7968
rcbuckner <rcbuckner@wisc.edu>
2023-11-26 14:53:23 -0600

    Updated UserDao select statements

--- app/src/main/java/com/cs407/madisonparkngo/UserDao.java
+++ app/src/main/java/com/cs407/madisonparkngo/UserDao.java
@@ -10,4 +10,22 @@ public interface UserDao {
     @Query("SELECT * FROM parkinglot")
     List<ParkingLot> getAllLots();
 
+    @Query("SELECT * FROM parkinglot WHERE motorcycle = 1")
+    List<ParkingLot> getMotorcycleLots();
+
+    @Query("SELECT * FROM parkinglot WHERE car = 1")
+    List<ParkingLot> getCarLots();
+
+    @Query("SELECT * FROM parkinglot WHERE moped = 1")
+    List<ParkingLot> getMopedLots();
+
+    @Query("SELECT * FROM parkinglot WHERE typeOfLot LIKE :lotType")
+    List<ParkingLot> getTypeOfLot(String lotType);
+
+    @Query("SELECT * FROM parkinglot WHERE permit LIKE :permit")
+    List<ParkingLot> getIfPermit(String permit);
+
+    @Query("SELECT * FROM parkinglot WHERE cost < :limit")
+    List<ParkingLot> getPricedLots(int limit);
+
 }
