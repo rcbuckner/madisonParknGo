7201f42ccf8565b75599013b4476600982314230
kcjones1832 <caseyhunterjones@gmail.com>
2023-11-28 15:48:52 -0600

    Revert "added csv file. added and finished a csv parser class."
    This reverts commit 9c3f16c89f7f6a8bc431d75588f6fdec600491fd.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,6 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.room:room-runtime:2.6.0")
     implementation("androidx.room:room-compiler:2.6.0")
-    implementation("androidx.room:room-runtime:2.6.0")
-    implementation("androidx.room:room-compiler:2.6.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
