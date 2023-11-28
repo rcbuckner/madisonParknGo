084fd316123e29f7d11851443e07fdc0653ec91f
idoll <idollevoet@wisc.edu>
2023-11-28 15:50:11 -0600

    Added back button functionality on lotInformation.java

--- app/src/main/java/com/cs407/madisonparkngo/lotInformation.java
+++ app/src/main/java/com/cs407/madisonparkngo/lotInformation.java
@@ -1,6 +1,10 @@
 package com.cs407.madisonparkngo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class lotInformation extends AppCompatActivity {
@@ -9,5 +13,15 @@ public class lotInformation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lot_information);
+
+        Button backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Navigate back to Map activity
+                Intent intent = new Intent(lotInformation.this, Map.class);
+                startActivity(intent);
+            }
+        });
     }
 }
