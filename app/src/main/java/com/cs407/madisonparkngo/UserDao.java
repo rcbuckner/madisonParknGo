d29750e0ff82fea1acae41b134e88c30650d61af
rcbuckner <rcbuckner@wisc.edu>
2023-11-30 13:40:50 -0600

    Updated getPricedLots function to accept a min and max limit.

--- app/src/main/java/com/cs407/madisonparkngo/UserDao.java
+++ app/src/main/java/com/cs407/madisonparkngo/UserDao.java
@@ -26,8 +26,8 @@ public interface UserDao {
     @Query("SELECT * FROM parkinglot WHERE permit LIKE :permit")
     List<ParkingLot> getIfPermit(String permit);
 
-    @Query("SELECT * FROM parkinglot WHERE cost < :limit")
-    List<ParkingLot> getPricedLots(int limit);
+    @Query("SELECT * FROM parkinglot WHERE cost < :maxLimit AND cost > :minLimit")
+    List<ParkingLot> getPricedLots(int maxLimit, int minLimit);
 
     @Query("SELECT * FROM parkinglot WHERE lotID = :id")
     ParkingLot getSpecificLOt(int id);
