090b27bb2f3116cbc26f0aa96b4cdae571121ab8
kcjones1832 <caseyhunterjones@gmail.com>
2023-12-05 15:33:45 -0600

    added method to get the distance from two points in lat/lon

--- app/src/main/java/com/cs407/madisonparkngo/Map.java
+++ app/src/main/java/com/cs407/madisonparkngo/Map.java
@@ -456,6 +456,17 @@ public class Map extends FragmentActivity {
         mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
     }
 
+    private float getDistance(float lat1, float lon1, float lat2, float lon2) {
+        final int R = 6371; // Radius of the earth
 
+        double latDistance = Math.toRadians(lat2 - lat1);
+        double lonDistance = Math.toRadians(lon2 - lon1);
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double distance = (R * c * 1000); // convert to meters
+        return (float)(distance * 0.000621371);
+    }
 
 }
